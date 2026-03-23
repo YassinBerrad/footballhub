906883ec2d143bf5b2c47ca18f8f6fd2b6ba5875
Yassin El Berrad <r1007706@student.thomasmore.be>
2026-03-23 14:07:01 +0100

    project start

---
+++ src/main/java/be/thomasmore/footballhub/FootballhubApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.footballhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/footballhub/FootballhubApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.footballhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
